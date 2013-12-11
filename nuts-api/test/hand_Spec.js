3ba677faebda602000c105c9a976a1537ebf0f56
David Bruant <bruant.d@gmail.com>
2013-12-11 18:21:13 +0100

    Update hand_Spec.js

--- nuts-api/test/hand_Spec.js
+++ nuts-api/test/hand_Spec.js
@@ -1,9 +1,11 @@
 var assert = require("assert");
+var Hand = require("../src/hand");
+
 describe('Hand', function(){
 	it('should throw an exception when the cards are identical', function(){
 		//Spade, Heart, Diamond, Club
 		// ♠ ♥ ♦ ♣
-		Hand = require("../src/hand");
+		
 		assert.throw(new Hand("5-S", "5-S"), Error);
 	});
 });
